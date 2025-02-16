7c03df066ad157642f0098ad7166a8b94814ac2f
Giacomo Lavermicocca <giacomo.cervignano@gmail.com>
2025-02-16 16:56:14 +0100

    vercel + lint

--- src/app/[language]/hours-history/page-content.tsx
+++ src/app/[language]/hours-history/page-content.tsx
@@ -6,18 +6,45 @@ import { useTranslation } from "@/services/i18n/client";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import React from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs, { Dayjs } from "dayjs";
+import useLanguage from "@/services/i18n/use-language";
+import "dayjs/locale/it";
+import "dayjs/locale/en";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 function AdminPanel() {
   const { t } = useTranslation("admin-panel-home");
 
+  const language = useLanguage();
+
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs());
+
   return (
     <Container maxWidth="md">
-      <Grid container spacing={3} wrap="nowrap" pt={3}>
+      <Grid
+        container
+        direction="column"
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Grid>
-          <Typography variant="h3" gutterBottom>
-            STORICO {t("title")}
-          </Typography>
-          <Typography>{t("description")}</Typography>
+          <LocalizationProvider
+            dateAdapter={AdapterDayjs}
+            adapterLocale={language}
+          >
+            <DatePicker
+              label="Date Picker"
+              format="YYYY/MM/DD"
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+            />
+          </LocalizationProvider>
         </Grid>
       </Grid>
     </Container>

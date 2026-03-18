9bdc83e62afe09e2688b8680906d3567d856aa6a
Giacomo Lavermicocca <giacomo.cervignano@gmail.com>
2026-03-18 11:47:49 +0100

    Fix traduzioni

--- src/app/[language]/hours/manage/create/[type]/page.tsx
+++ src/app/[language]/hours/manage/create/[type]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   const params = await props.params;
   const { t } = await getServerTranslation(
     params.language,
-    "admin-panel-users-create"
+    "admin-panel-hours-create"
   );
 
   return {
